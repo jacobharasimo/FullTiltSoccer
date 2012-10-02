2c23c30a753326a85f4fda81c880a9d61adad3e6
Jacob Harasimo <jacobharasimo@gmail.com>
2012-10-02 07:32:12 -0400

    asp.net mvc 4 setup

--- Model/DataAccessService.cs
+++ Model/DataAccessService.cs
@@ -18,21 +18,55 @@ namespace FullTiltSoccerRS
     // NOTE: If the service is renamed, remember to update the global.asax.cs file
     public class DataAccessService
     {
-        readonly FullTiltSoccerDataContext _dataConnection = new FullTiltSoccerDataContext();
-        [WebGet(UriTemplate = "GetAllTournaments", ResponseFormat=WebMessageFormat.Json)]
-        public List<GetAllTournamentsResult> GetCollection()
-        {            
+        readonly FullTiltSoccerDataContext _dataConnection = new FullTiltSoccerDataContext();        
+
+        [WebGet(UriTemplate = "GetAllActiveCoachesCorner?date={date}", ResponseFormat = WebMessageFormat.Json)]
+        public List<GetAllActiveCoachesCornerListingsResult> GetAllActiveCoachesCorner(DateTime date)
+        {
+            return new List<GetAllActiveCoachesCornerListingsResult>(_dataConnection.GetAllActiveCoachesCornerListings(date));
+        }
+
+        [WebGet(UriTemplate = "GetAllActiveTournaments?date={date}", ResponseFormat = WebMessageFormat.Json)]
+        public List<GetAllActiveTournamentsResult> GetAllActiveTournaments(DateTime date)
+        {
+            return new List<GetAllActiveTournamentsResult>(_dataConnection.GetAllActiveTournaments(date));
+        }
+
+        [WebGet(UriTemplate = "GetAllCoachesCornerListings", ResponseFormat = WebMessageFormat.Json)]
+        public List<GetAllCoachesCornerListingsResult> GetAllCoachesCornerListings()
+        {
+            return new List<GetAllCoachesCornerListingsResult>(_dataConnection.GetAllCoachesCornerListings());
+        }
+
+        [WebGet(UriTemplate = "GetAllTournaments", ResponseFormat = WebMessageFormat.Json)]
+        public List<GetAllTournamentsResult> GetAllTournaments()
+        {
             return new List<GetAllTournamentsResult>(_dataConnection.GetAllTournaments());
         }
 
-        [WebGet(UriTemplate = "test", ResponseFormat = WebMessageFormat.Json)]
-        public List<string> Test()
+        [WebGet(UriTemplate = "GetAllTournamentsByUser?ID={id}", ResponseFormat = WebMessageFormat.Json)]
+        public List<GetAllTournamentsByUserResult> GetAllTournamentsByUser(int? id)
         {
-            return new List<string>
-                {
-                    {"Test1"},
-                    {"Test2"}
-                };
+            return  new List<GetAllTournamentsByUserResult>(_dataConnection.GetAllTournamentsByUser(id));
         }
+
+        [WebGet(UriTemplate = "GetCmsDataById?ID={id}", ResponseFormat = WebMessageFormat.Json)]
+        public CmsData GetCmsDataById(string id)
+        {           
+            return _dataConnection.GetCMSDataById(id).First();
+        }
+
+        [WebGet(UriTemplate = "GetCoachesCornerRegionlisting?Date={currentDate}&Country={countryId}&Region={regionId}", ResponseFormat = WebMessageFormat.Json)]
+        public List<GetCoachesCornerRegionListingResult> GetCoachesCornerRegionlisting(DateTime currentDate, int? countryId, int? regionId)
+        {
+            return new List<GetCoachesCornerRegionListingResult>(_dataConnection.GetCoachesCornerRegionListing(currentDate, countryId, regionId));
+        }
+
+        [WebGet(UriTemplate = "GetCoachesCornerRegionlistingByAge?Date={currentDate}&Country={countryId}&Region={regionId}&Age={age}", ResponseFormat = WebMessageFormat.Json)]
+        public List<GetCoachesCornerRegionListingByAgeResult> GetCoachesCornerRegionlistingByAge(DateTime currentDate, int? countryId, int? regionId, int age)
+        {                        
+            return new List<GetCoachesCornerRegionListingByAgeResult>(_dataConnection.GetCoachesCornerRegionListingByAge(currentDate, countryId, regionId, age));
+        }
+       
     }
 }
---
+++ Web/Controllers/TournamentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class TournamentController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
