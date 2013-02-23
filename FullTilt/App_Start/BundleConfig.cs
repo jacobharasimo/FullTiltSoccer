8346f4f74440782d9e6119093b2b19cb696edb6f
jharasimo <jacobharasimo@gmail.com>
2013-02-23 15:55:47 -0500

    more framework

--- FullTilt/App_Start/BundleConfig.cs
+++ FullTilt/App_Start/BundleConfig.cs
@@ -30,9 +30,17 @@ namespace FullTilt
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/fancybox").Include(
+                "~/Scripts/fancybox/jquery.fancybox.js",
+                "~/Scripts/fancybox/jquery.fancybox.pack.js"));
+
+
             bundles.Add(new StyleBundle("~/Style/foundation").Include(
                 "~/Stylesheets/foundation.min.css",
                 "~/Stylesheets/app.css"));
+
+            bundles.Add(new StyleBundle("~/Style/fancybox").Include(
+               "~/Scripts/fancybox/jquery.fancybox.css"));
           
             BundleTable.EnableOptimizations = true;
         }
